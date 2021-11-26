90a1fca40bf723a3be3411b5b91416491b35aa8b
TextError <savinraul1983@yahoo.com>
2021-11-26 11:15:02 +0000

    Computed func

--- app.js
+++ app.js
@@ -2,14 +2,33 @@ const vm = Vue.createApp({
   data() {
     return {
       f_name: 'John',
+      m_name: '',
       l_name: 'Doe',
       url: 'https://google.com',
-      raw_url: "<a href='https://google.com'  target='_blank'>Google</a>"
+      raw_url: "<a href='https://google.com'  target='_blank'>Google</a>",
+      age: 20
     }
   },
   methods: {
     name() {
       return `${this.f_name} ${this.l_name}`
+    },
+    updateMiddleName({ target: { value }}) {
+      this.m_name = value
+    },
+    updateLName({ target: { value }}) {
+      this.l_name = value
+    },
+    increment() {
+      this.age++
+    },
+    add(value) {
+      this.age = this.age + value
+    }
+  },
+  computed: {
+    fullName() {
+      return `${this.f_name} ${this.m_name} ${this.l_name}`
     }
   }
 }).mount('#app')
