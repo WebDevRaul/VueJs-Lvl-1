d2e9440d804b0e8132505aa860abf771063b2362
TextError <savinraul1983@yahoo.com>
2021-11-26 08:14:09 +0000

    Use on javascript methods inline

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-Vue.createApp({}).mount('#app')
+const vm = Vue.createApp({
+  data() {
+    return {
+      f_name: 'John',
+      l_name: 'Doe'
+    }
+  }
+}).mount('#app')
+
+// setTimeout(() => { vm.f_name = 'Bob' }, 2000)
