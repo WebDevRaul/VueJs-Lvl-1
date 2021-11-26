c9e7de40337c7f7dd6d091f38afa2738cfbe228c
TextError <savinraul1983@yahoo.com>
2021-11-26 21:22:18 +0000

    Basic components

--- app.js
+++ app.js
@@ -1,32 +1,45 @@
 const vm = Vue.createApp({
+  // data() {
+  //   return {
+  //     message: "Hello world!"
+  //   }
+  // },
+  
+  // beforeCreate() {
+  //   console.log("beforeCreate() func called!", this.message)
+  // },
+  // created() {
+  //   console.log("created() func called!", this.message)
+  // },
+  // beforeMount() {
+  //   console.log("beforeMount() func called!", this.$el)
+  // },
+  // mounted() {
+  //   console.log("mounted() func called!", this.el)
+  // },
+  // beforeUpdate() {
+  //   console.log("beforeUpdate() func called!")
+  // },
+  // updated() {
+  //   console.log("updated() func called!")
+  // },
+  // beforeUnmount() {
+  //   console.log("beforeUnmount() func called!")
+  // },
+  // unmounted() {
+  //   console.log("unmounted() func called!")
+  // }
+
+  // template: `{{ message }}`
+
+});
+
+vm.component("appComponent", {
+  template: `<h1>{{ message }}</h1>`,
   data() {
     return {
-      message: "Hello world!"
+      message: 'Hello world!'
     }
-  },
-  beforeCreate() {
-    console.log("beforeCreate() func called!", this.message)
-  },
-  created() {
-    console.log("created() func called!", this.message)
-  },
-  beforeMount() {
-    console.log("beforeMount() func called!", this.$el)
-  },
-  mounted() {
-    console.log("mounted() func called!", this.el)
-  },
-  beforeUpdate() {
-    console.log("beforeUpdate() func called!")
-  },
-  updated() {
-    console.log("updated() func called!")
-  },
-  beforeUnmount() {
-    console.log("beforeUnmount() func called!")
-  },
-  unmounted() {
-    console.log("unmounted() func called!")
   }
 });
 
