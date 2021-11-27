9a5c066352a3fb179d7baa0b9a7ddb6722272a19
TextError <savinraul1983@yahoo.com>
2021-11-27 06:37:56 +0000

    webpack + babel + scss config

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,32 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 module.exports = {
   entry: './webpack/index.js', // or no entry option if src/index.js
   output: {
     filename: 'bundle.js',
     path: __dirname + '/dist'
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js%/,
+        exclude: /(node_modules)/,
+        use: 'babel-loader'
+      },
+      // Sass 
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader, // or 'style-loader' for plain style css in head html file
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'main.css'   
+    })
+  ]
 }
